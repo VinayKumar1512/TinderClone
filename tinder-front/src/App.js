a63370949e019fdec3e05e0033185840b366302c
Vinay Kumar <79040443+VinayKumar1512@users.noreply.github.com>
2021-04-25 14:28:59 +0530

    Add files via upload

---
+++ tinder-front/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Header from "./Header";
+import TinderCards from "./TinderCards";
+import SwipeButtons from "./SwipeButtons";
+
+function App() {
+  return (
+    <div className="app">
+ 
+  {/* Header */}
+  <Header/>
+  {/* TinderCards */}
+  <TinderCards/>
+  {/* SwipeButtons */}
+  <SwipeButtons/>
+  
+    </div>
+  );
+}
+
+export default App;
